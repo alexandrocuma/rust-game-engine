1a893a4567726442fe3c422f1f92fb19b6d0e1a3
Alexandro Cu <alexandro.cuma@gmail.com>
2023-08-01 23:06:21 -0600

    feat(draw): Added draw animation of Square

--- examples/game.rs
+++ examples/game.rs
@@ -1,16 +1,29 @@
 use game_engine::*;
 fn main() {
-  let mut x_position = 200.0;
-  let mut y_position = 30.0;
+  let mut x_position = 200.;
+  let mut y_position = 30.;
 
-  set_event_handler(move |key| { 
-    let move_amount = 20.0;
-    match key {
-      Key::Left => clear_screen_to_color(1.0, 0.0, 0.0, 1.0),
-      Key::Right => clear_screen_to_color(0.0, 1.0, 0.0, 1.0),
-      Key::Up => clear_screen_to_color(0.0, 0.0, 1.0, 1.0),
-      Key::Down => clear_screen_to_color(0.0, 1.0, 1.0, 1.0),
-      Key::Space => clear_screen_to_color(1.0, 1.0, 0.0, 1.0),
+  let mut x_direction = 1.;
+  let mut y_direction = 1.;
+
+  let speed = 5.;
+  set_event_handler(move |event| { 
+    match event {
+      Event::Draw => {
+        x_position += x_direction * speed;
+        y_position += y_direction * speed;
+
+        if x_position < 0. || x_position > 1000. {
+          x_direction *= -1.;
+        }
+        if y_position < 0. || y_position > 700. {
+          y_direction *= -1.;
+        }
+
+        clear_screen_to_color(0.0, 0.0, 0.3, 1.0);
+        draw_rectangle(x_position, y_position, 100., 100.);
+      }
+      _ => {}
     }
 
     clear_screen_to_color(0.0, 0.0, 0.3, 1.0);
