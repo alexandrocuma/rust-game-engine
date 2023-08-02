63a0f5a3c283ca5b0a2e529503aae85a11c1acc7
Alexandro Cu <alexandro.cuma@gmail.com>
2023-08-02 12:53:00 -0600

    feat(rectangle): Added Rectangle movement and Stop

--- examples/game.rs
+++ examples/game.rs
@@ -6,27 +6,32 @@ fn main() {
   let mut x_direction = 1.;
   let mut y_direction = 1.;
 
-  let speed = 5.;
-  set_event_handler(move |event| { 
+  let mut speed = 5.;
+  set_event_handler(move |context, event| { 
     match event {
       Event::Draw => {
         x_position += x_direction * speed;
         y_position += y_direction * speed;
 
-        if x_position < 0. || x_position > 1000. {
+        if x_position < 0. || x_position > 1350. {
           x_direction *= -1.;
         }
         if y_position < 0. || y_position > 700. {
           y_direction *= -1.;
         }
 
-        clear_screen_to_color(0.0, 0.0, 0.3, 1.0);
-        draw_rectangle(x_position, y_position, 100., 100.);
+        context.clear_screen_to_color(0., 0., 0.3, 1.);
+        context.draw_rectangle(x_position, y_position, 100., 100., 1., 0., 0., 1.);
       }
-      _ => {}
+      Event::KeyDown(key) => {
+        match key {
+          Key::Right => x_position += 10.0,
+          Key::Left => x_position -= 10.0,
+          Key::Up => y_position += 10.0,
+          Key::Down => y_position -= 10.0,
+          Key::Space => speed = 0. 
+        }
+      }
     }
-
-    clear_screen_to_color(0.0, 0.0, 0.3, 1.0);
-    draw_rectangle(x_position, y_position, 100., 100.);
   }) 
 }
